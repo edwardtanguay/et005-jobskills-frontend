95bf375e7854148bb205c6f3743db8b385c1fd7e
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-21 08:48:03 +0200

    changes

--- src/pages/PageJobs.tsx
+++ src/pages/PageJobs.tsx
@@ -1,7 +1,22 @@
+import { useContext } from 'react';
+import { AppContext } from '../AppContext';
+
 export const PageJobs = () => {
+	const { jobs } = useContext(AppContext);
+
+	const _jobs = jobs.filter(m => m.publicationDate > '2023-03-01').sort((a, b) => (a.publicationDate < b.publicationDate) ? 1 : 0);
+
 	return (
-		<>
-			<p>This is the jobs page.</p>
-		</>
+		<div className="pageWelcome">
+			<div className="jobs">
+				{_jobs.map((job) => {
+					return <div className="job">
+						<div className="publicationDate">{job.publicationDate}</div>
+						<div className="title"><a target="_blank" href={job.url}>{job.title}</a> - {job.company}</div>
+						<div className="skillList">{job.skillList}</div>
+					</div>;
+				})}
+			</div>
+		</div>
 	);
 };
