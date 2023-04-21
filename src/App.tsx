6936ff93ab7b127787526749433a234213fd3f83
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-21 06:14:18 +0200

    nav pages in

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.scss';
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
+import { PageWelcome } from './pages/PageWelcome';
+import { PageJobs } from './pages/PageJobs';
+import { PageSkills } from './pages/PageSkills';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Job Site</h1>
-      <p>Welcome to this site.</p>
+			<nav>
+				<NavLink to="/welcome">Welcome</NavLink>
+				<NavLink to="/jobs">Jobs</NavLink>
+				<NavLink to="/skills">Skills</NavLink>
+			</nav>
+
+			<Routes>
+				<Route path="/welcome" element={<PageWelcome />} />
+				<Route path="/jobs" element={<PageJobs />} />
+				<Route path="/skills" element={<PageSkills />} />
+				<Route path="/" element={<Navigate to="/germany" replace />} />
+			</Routes>
 		</div>
 	);
 }
