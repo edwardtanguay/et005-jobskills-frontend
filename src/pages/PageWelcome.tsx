f4b22b8f8f3c750edfb4b4466ff51c22536f4588
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-21 08:11:00 +0200

    welcome page in

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
+import { AppContext } from '../AppContext';
+
 export const PageWelcome = () => {
+	const { jobs } = useContext(AppContext);
+
 	return (
 		<>
-			<p>This is the welcome page.</p>
+			<p>There are {jobs.length} jobs.</p>
 		</>
 	);
 };
